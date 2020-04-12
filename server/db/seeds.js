e3d66265dfaa6d01c809f49287168e6b9a3ed3dd
junwu <junwu2012@hotmail.com>
2020-04-12 23:31:34 +0100

    change to real address with lat lon

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -14,9 +14,11 @@ db.needs.insertMany([
       contactNumber: "07711666577",
       email: "johnboy@hotmail.com",
       address: "4/7 Corstorphine High Street",
-      postCode: "E12 7TQ",
+      postCode: "EH12 7ST",
       time: "17:00",
-      date: "06/6/2020"
+      date: "06/6/2020",
+      lat: "55.94084843785257",
+      lon: "-3.2828476938395688"
     }
   },
 
@@ -30,10 +32,12 @@ db.needs.insertMany([
     contactDetails: {
       contactNumber: "07778346565",
       email: "Amyaims@hotmail.com",
-      address: "1 Union Street",
-      postCode: "E12 3HU",
+      address: "1 Grange Rd Edinburgh",
+      postCode: "EH9 1UH",
       time: "06:00",
-      date: "12/5/2020"
+      date: "12/5/2020",
+      lat: "55.9368168",
+      lon: "-3.1806435"
     }
   },
 
@@ -47,10 +51,12 @@ db.needs.insertMany([
     contactDetails: {
       contactNumber: "07711666577",
       email: "johnboy@hotmail.com",
-      address: "6 - 10 Earl Grey Street",
+      address: "10 Earl Grey Street Edinburgh",
       postCode: "EH3 9BN",
       time: "10:00",
-      date: "27/5/2020"
+      date: "27/5/2020",
+      lat: "55.9439431",
+      lon: "-3.2041938"
     }
   },
 ]);
