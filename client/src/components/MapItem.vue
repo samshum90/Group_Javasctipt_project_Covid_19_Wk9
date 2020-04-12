535663911c2c59581c1c585382fb218cbb1b909e
Sam <samhum90@gmail.com>
2020-04-12 23:48:48 +0100

    first try

--- client/src/components/MapItem.vue
+++ client/src/components/MapItem.vue
@@ -1,20 +1,23 @@
 <template>
     <div class="map-container">
-    
-    <l-map class="map"
-      :zoom="zoom"
-      :center="center"
-      @update:zoom="zoomUpdated"
-      @update:center="centerUpdated"
-      @update:bounds="boundsUpdated"
-    >
-      <l-tile-layer :url="url"></l-tile-layer>
+      <l-map class="map"
+        :zoom="zoom"
+        :center="center"
+        @update:zoom="zoomUpdated"
+        @update:center="centerUpdated"
+        @update:bounds="boundsUpdated"
+      >
+        <l-tile-layer :url="url"></l-tile-layer>
         <l-marker :lat-lng="markerLatLng" ></l-marker>
-        <div v-for="(need, index) in needs" :need="need" :key="index">
-          <p>hello</p>
+        <div v-for="(location, index) in needsLocations" :location="location" :key="index">
+          <l-marker :lat-lng="location"></l-marker>
         </div>
-    </l-map>
+        
+      </l-map>
+    
     </div>
+ 
+   
 </template>
 
 <script>
@@ -30,12 +33,16 @@ export default {
   props: ['needs'],
   data () {
     return {
-      name: "map",
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       zoom: 13,
       center: [55.94100, -3.20356],
       bounds: null,
-      markerLatLng: [55.94100, -3.20356]
+      markerLatLng: [55.94100, -3.20356],
+      needsLocations: [
+        [55.9423682,-3.2683761],
+        [55.94692396000001,-3.20235642]
+      ],
+      tmpLatLng: null
     };
   },
   mounted() {
@@ -44,14 +51,10 @@ export default {
         iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
         iconUrl: require('leaflet/dist/images/marker-icon.png'),
         shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-        });
+});
   },
-  computed() {
-    // convertPostcode(postcode) {
-    //   for( need in this.need ){
-        
-    //   }
-    // }
+  computed: {
+    
   },
   methods: {
     zoomUpdated (zoom) {
@@ -62,12 +65,28 @@ export default {
     },
     boundsUpdated (bounds) {
       this.bounds = bounds;
+    },
+    getLatLng: function(address){
+      const url = 'https://nominatim.openstreetmap.org/search?format=json&q='+address;
+      let location;
+      let result = fetch(url)
+      .then( res => res.json())
+      .then( location => {
+        location = "["+location[0].lat + ", "+location[0].lon + "]";
+        this.tmpLatLng = location;
+        console.log(location);
+        // return location;
+      });
+      // console.log(result);
+        // console.log(`location outside:${location}`);
+      return this.tmpLatLng;    
+    },
+    getName: function(name){
+      return "Hello "+name;
     }
+    
   }
 }
-
-
-
 </script>
 
 <style>
