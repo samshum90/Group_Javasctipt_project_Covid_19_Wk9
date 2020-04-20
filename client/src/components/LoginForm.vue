d51584d62056c0a8040f523a03850cf9ad905879
Anna <elbereth_spencer@hotmail.com>
2020-04-20 19:12:10 +0100

    adding git ignore at the parent folder

---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div  class="login">
+    <form action="http://localhost:3000/api/users" method="post">
+  
+  <div class="container">
+    <label for="name"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="name" required>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+    
+  </div>
+
+</form>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"login-form"
+}
+
+</script>
+
+<style>
+
+
+
+</style>
